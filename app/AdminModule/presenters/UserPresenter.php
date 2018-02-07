fecc26e0f03858773dddcd60853d15871b0abc82
rendi <rendi@DESKTOP-AFFU7Q7>
2018-02-07 21:19:43 +0100

    admin/user show some adittiona informations

--- app/AdminModule/presenters/UserPresenter.php
+++ app/AdminModule/presenters/UserPresenter.php
@@ -32,9 +32,10 @@ class UserPresenter extends Base\AdminPresenter {
         $form->addText('user_name', 'User name:')->setRequired(true);
         $form->addEmail('user_email', 'User mail:')->setRequired(true);
         $form->addSelect('user_role_id', 'User role:', $this->rolesManager->getForSelect())->setTranslator(null)->setRequired(true);
+        $form->addSelect('user_lang_id', 'User language:', $this->languagesManager->getAllForSelect());
         $form->addTextArea('user_signature', 'User signature:');        
         //$form->addUpload('user_avatar', 'User avatar:');
-        $form->addSelect('user_lang_id', 'User language:', $this->languagesManager->getAllForSelect());
+
         $form->addCheckbox('user_active', 'User active:');
         
         
