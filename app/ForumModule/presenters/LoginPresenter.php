4ffff06ad67b7260e4fc1d575282bfd6ce37e968
rendix2 <rendix2@seznam.cz>
2018-09-24 23:34:59 +0200

    user resend activate mail #63

--- app/ForumModule/presenters/LoginPresenter.php
+++ app/ForumModule/presenters/LoginPresenter.php
@@ -2,11 +2,17 @@
 
 namespace App\ForumModule\Presenters;
 
+use App\Controls\BBMailer;
+use App\Controls\BootstrapForm;
+use App\Models\Manager;
 use App\Models\UsersManager;
 use App\Presenters\Base\BasePresenter;
 use App\Services\UserLoginFormFactory;
 use App\Forms\UserLoginForm;
+use App\Translator;
+use Nette\Application\UI\Form;
 use Nette\Utils\ArrayHash;
+use Tracy\Debugger;
 
 /**
  * Description of LoginPresenter
@@ -33,7 +39,28 @@ class LoginPresenter extends BasePresenter
      * @inject
      */
     public $usersManager;
-    
+
+    /**
+     * @var BBMailer $bbMailer
+     * @inject
+     */
+    public $bbMailer;
+
+    /**
+     * @var Translator $translator
+     */
+    private $translator;
+
+    /**
+     * LoginPresenter constructor.
+     */
+    public function startup()
+    {
+        parent::startup();
+
+        $this->translator = $this->translatorFactory->forumTranslatorFactory();
+    }
+
     /**
      *
      * @param mixed $element
@@ -94,6 +121,11 @@ class LoginPresenter extends BasePresenter
             $this->redirect('Index:default');
         }
     }
+
+    public function renderReactivate()
+    {
+        // :)
+    }
     
     /**
      *
@@ -102,5 +134,49 @@ class LoginPresenter extends BasePresenter
     public function createComponentLoginForm()
     {
         return $this->userLoginForm->create();
+    }
+
+    protected function createComponentReactivateForm()
+    {
+        $form = BootstrapForm::create();
+
+        $form->addEmail('user_email', 'User email:');
+        $form->addSubmit('submit', 'Send');
+        $form->onSuccess[] = [$this, 'reactivateFormSuccess'];
+
+        return $form;
+    }
+
+    public function reactivateFormSuccess(Form $form, ArrayHash $values)
+    {
+        $user = $this->usersManager->getByEmail($values->user_email);
+
+        if ($user) {
+            $user = $user[0];
+
+            if (!$user->user_active) {
+                $key = Manager::getRandomString();
+
+                $this->usersManager->update($user->user_id, ArrayHash::from(['user_activation_key' => $key]));
+
+                $this->bbMailer->addRecipients([$values->user_email]);
+                $this->bbMailer->setSubject($this->translator->translate('welcome_mail_subject'));
+                $this->bbMailer->addRecipients([$values->user_email]);
+                $this->bbMailer->setText(
+                    sprintf(
+                        $this->translator->translate('welcome_mail_text'),
+                        $user->user_name,
+                        $this->link(
+                            '//Login:activate',
+                            $user->user_id,
+                            $key
+                        )
+                    )
+                );
+                $this->bbMailer->send();
+            }
+        }
+
+        $this->flashMessage('OK', self::FLASH_MESSAGE_SUCCESS);
     }
 }
