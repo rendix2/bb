aa8f370a4bab4a1a26eac464365df99818d54419
rendix2 <rendix2@seznam.cz>
2018-10-04 18:03:18 +0200

    save data into entity #160

--- app/presenters/Base/BasePresenter.php
+++ app/presenters/Base/BasePresenter.php
@@ -10,6 +10,10 @@ use App\Services\TranslatorFactory;
 use Nette;
 use Nette\Http\IResponse;
 use Nextras\Application\UI\SecuredLinksPresenterTrait;
+use App\Models\PostsManager;
+use App\Models\TopicsManager;
+use App\Models\ForumsManager;
+use App\Models\CategoriesManager;
 
 /**
  * Base presenter for all application presenters.
@@ -70,6 +74,34 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
      */
     public $translatorFactory;
     
+    /**
+     * @var CategoriesManager $categoriesManager
+     * @inject
+     */
+    public $categoriesManager;     
+    
+    /**
+     * @var ForumsManager $forumsManager
+     * @inject
+     */
+    public $forumsManager;  
+    
+    /**
+     * @var TopicsManager $topicsManager
+     * @inject
+     */
+    public $topicsManager;    
+    
+    /**
+     *
+     * @var PostsManager $manager
+     * @inject
+     */
+    public $postsManager;
+    
+
+
+
     /**
      * BasePresenter constructor.
      */
@@ -209,4 +241,133 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 
         return new MenuControl($this->translatorFactory->adminTranslatorFactory(), $leftMenu, $rightMenu);
     }
+    
+    public function checkCategoryParam($category_id)
+    {
+        // category check
+        if (!isset($category_id)) {
+            $this->error('Category param is not set.');
+        }
+
+        if (!is_numeric($category_id)) {
+            $this->error('Category param is not numeric.');
+        }
+
+        $category = $this->categoriesManager->getById($category_id);
+
+        if (!$category) {
+            $this->error('Category was not found.');
+        }
+
+        if (!$category->category_active) {
+            $this->error('Category is not active.');
+        }
+
+        return $category;
+    }
+    
+    public function checkForumParam($forum_id, $category_id)
+    {
+        // forum check
+        if (!isset($forum_id)) {
+            $this->error('Forum param is not set.');
+        }
+
+        if (!is_numeric($forum_id)) {
+            $this->error('Forum param is not numeric.');
+        }
+
+        $forum = $this->forumsManager->getById($forum_id);
+
+        if (!$forum) {
+            $this->error('Forum was not found.');
+        }
+
+        if ($forum->forum_category_id !== (int)$category_id) {
+            $this->error('Category param does not match.');
+        }
+
+        if (!$forum->forum_active) {
+            $this->error('Forum is not active.');
+        }
+
+        return $forum;
+    }    
+
+    public function checkTopicParam($topic_id, $category_id, $forum_id)
+    {
+        // topic check
+        if (!isset($topic_id)) {
+            $this->error('Topic param is not set.');
+        }
+
+        if (!is_numeric($topic_id)) {
+            $this->error('Topic param is not numeric.');
+        }
+
+        $topicDibi = $this->topicsManager->getById($topic_id);
+        
+        if (!$topicDibi) {
+            $this->error('Topic was not found.');
+        }
+        
+        $topic = \App\Models\Entity\Topic::get($topicDibi);
+
+        if ($topic->topic_category_id !== (int)$category_id) {
+            $this->error('Category param does not match.');
+        }
+
+        if ($topic->topic_forum_id !== (int)$forum_id) {
+            $this->error('Forum param does not match.');
+        }
+
+        if ($topic->topic_locked) {
+            $this->error('Topic is locked.', IResponse::S403_FORBIDDEN);
+        }
+        
+        return $topic;
+    }
+    
+    public function checkPostParam($post_id, $category_id, $forum_id, $topic_id)
+    {
+        if (!isset($post_id)) {
+            $this->error('Post param is not set.');
+        }
+        
+        if (!is_numeric($post_id)) {
+            $this->error('Post param is not numeric.');
+        }        
+
+        $postDibi = $this->postsManager->getById($post_id);
+
+        if (!$postDibi) {
+            $this->error('Post was not found.');
+        }
+        
+        $post = \App\Models\Entity\Post::get($postDibi);        
+
+        if ($post->post_category_id !== (int)$category_id) {
+            $this->error('Category param does not match.', IResponse::S403_FORBIDDEN);
+        }
+
+        if ($post->post_forum_id !== (int)$forum_id) {
+            $this->error('Category param does not match.', IResponse::S403_FORBIDDEN);
+        }
+
+        if ($post->post_topic_id !== (int)$topic_id) {
+            $this->error('Category param does not match.', IResponse::S403_FORBIDDEN);
+        }
+
+        if ($post->post_user_id !== $this->getUser()->getId()) {
+            $this->error('You are not author of post.', IResponse::S403_FORBIDDEN);
+        }
+
+        if ($post->post_locked) {
+            $this->error('Post is locked.', IResponse::S403_FORBIDDEN);
+        }
+
+        return $post;
+    }
+    
+
 }
