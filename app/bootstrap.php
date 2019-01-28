823a54579e3528e17cda338accbc07b29a99c29f
Tomáš Babický <tomas.babicky@websta.de>
2019-01-28 13:48:31 +0100

    clean #129

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,13 +9,17 @@ $configurator = new Nette\Configurator;
 $log_dir           = __DIR__ . '/../log';
 $temp_dir          = __DIR__ . '/../temp';
 $temp_sessions_dir = __DIR__ . '/../temp/sessions';
+$config_dir        = __DIR__ . '/../config';
+
+// check if all dirs exists first
+
+if (!file_exists($config_dir) && !is_dir($config_dir)) {
+    mkdir($config_dir);
+}
 
 if (!file_exists($log_dir) && !is_dir($log_dir)) {
     mkdir($log_dir);
 }
-
-$configurator->enableTracy($log_dir);
-$configurator->setTimeZone('Europe/Prague');
 
 if (!file_exists($temp_dir) && !is_dir($temp_dir)) {
     mkdir($temp_dir);
@@ -25,16 +29,18 @@ if (!file_exists($temp_sessions_dir) && !is_dir($temp_sessions_dir)) {
     mkdir($temp_sessions_dir);
 }
 
+$configurator->enableTracy($log_dir);
+$configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory($temp_dir);
 
 $configurator->createRobotLoader()
     ->addDirectory(__DIR__)
     ->register();
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
+$configurator->addConfig($config_dir . '/config.neon');
 
 if (!Tracy\Debugger::$productionMode) {
-    $configurator->addConfig(__DIR__ . '/config/config.local.neon');
+    $configurator->addConfig($config_dir . '/config.local.neon');
 }
 
 $container = $configurator->createContainer();
